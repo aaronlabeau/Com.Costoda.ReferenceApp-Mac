561be9c07e65a87d0f9db14c7dbd8d57ce08abbe
Aaron LeBeau <aaron.lebeau@ey.com>
2020-03-02 11:21:33 -0500

    Initial commit

---
+++ Com.Costoda.ReferenceApp.Mac/Main.cs
@@ -0,0 +1,13 @@
+﻿using AppKit;
+
+namespace Com.Costoda.ReferenceApp.Mac
+{
+    static class MainClass
+    {
+        static void Main(string[] args)
+        {
+            NSApplication.Init();
+            NSApplication.Main(args);
+        }
+    }
+}
